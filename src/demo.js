d2fa34e28dbe7b54a9c6c20e03c09a20577150c1
Bernhard Jahn <bernhard.jahn@googlemail.com>
2021-05-30 10:48:54 +0200

    change to functional component

--- src/demo.js
+++ src/demo.js
@@ -1,127 +1,101 @@
-import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { connectProps } from '@devexpress/dx-react-core';
-import {
-  EventTracker, HoverState,
-} from '@devexpress/dx-react-chart';
+import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import { connectProps } from "@devexpress/dx-react-core";
+import { EventTracker, HoverState } from "@devexpress/dx-react-chart";
 import {
   Chart,
   SplineSeries,
   ArgumentAxis,
   ValueAxis,
-  Legend,
-} from '@devexpress/dx-react-chart-material-ui';
-import { energyConsumption as data } from './demo-data/data-vizualization';
+  Legend
+} from "@devexpress/dx-react-chart-material-ui";
+import { energyConsumption as data } from "../../../demo-data/data-vizualization";
 
 const rootStyles = {
-  display: 'flex',
-  margin: 'auto',
-  flexDirection: 'row',
+  display: "flex",
+  margin: "auto",
+  flexDirection: "row"
 };
-const LegendRoot = props => (
-  <Legend.Root
-    {...props}
-    style={rootStyles}
-  />
-);
+const LegendRoot = (props) => <Legend.Root {...props} style={rootStyles} />;
 
 const defaultLabelStyles = {
-  marginBottom: '8px',
-  whiteSpace: 'nowrap',
-  fontSize: '20px',
-  color: 'lightgray',
+  marginBottom: "8px",
+  whiteSpace: "nowrap",
+  fontSize: "20px",
+  color: "lightgray"
 };
 const hoveredLabelStyles = {
   ...defaultLabelStyles,
-  color: 'black',
+  color: "black"
 };
 const LegendLabel = ({ hoveredSeriesName, text }) => (
-  <div style={hoveredSeriesName === text ? hoveredLabelStyles : defaultLabelStyles}>
+  <div
+    style={hoveredSeriesName === text ? hoveredLabelStyles : defaultLabelStyles}
+  >
     {text}
   </div>
 );
 
 const itemStyles = {
-  flexDirection: 'column-reverse',
+  flexDirection: "column-reverse"
 };
-const LegendItem = props => (
-  <Legend.Item
-    {...props}
-    style={itemStyles}
-  />
-);
+const LegendItem = (props) => <Legend.Item {...props} style={itemStyles} />;
 
-export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
+export default (props) => {
+  const state = React.useState({
+    data,
+    hover: undefined
+  });
 
-    this.state = {
-      data,
-      hover: undefined,
+  const changeHover = React.useCallback(
+    (hover) => this.setState({ hover }),
+    []
+  );
+
+  const legendLabel = connectProps(LegendLabel, () => {
+    const { hover } = state;
+    const hoveredSeriesName = hover ? hover.series : undefined;
+    return {
+      hoveredSeriesName
     };
+  });
+  React.useEffect(() => legendLabel.update(), [legendLabel]);
 
-    this.changeHover = hover => this.setState({ hover });
+  const { data: chartData, hover } = state;
 
-    this.legendLabel = connectProps(LegendLabel, () => {
-      const { hover } = this.state;
-      const hoveredSeriesName = hover ? hover.series : undefined;
-      return {
-        hoveredSeriesName,
-      };
-    });
-  }
+  return (
+    <Paper>
+      <Chart data={chartData}>
+        <ArgumentAxis />
+        <ValueAxis />
 
-  componentDidUpdate() {
-    this.legendLabel.update();
-  }
+        <SplineSeries
+          name="Hydro-electric"
+          valueField="hydro"
+          argumentField="country"
+        />
+        <SplineSeries name="Oil" valueField="oil" argumentField="country" />
+        <SplineSeries
+          name="Natural gas"
+          valueField="gas"
+          argumentField="country"
+        />
+        <SplineSeries name="Coal" valueField="coal" argumentField="country" />
+        <SplineSeries
+          name="Nuclear"
+          valueField="nuclear"
+          argumentField="country"
+        />
+        <Legend
+          position="bottom"
+          rootComponent={LegendRoot}
+          itemComponent={LegendItem}
+          labelComponent={legendLabel}
+        />
 
-  render() {
-    const { data: chartData, hover } = this.state;
-
-    return (
-      <Paper>
-        <Chart
-          data={chartData}
-        >
-          <ArgumentAxis />
-          <ValueAxis />
-
-          <SplineSeries
-            name="Hydro-electric"
-            valueField="hydro"
-            argumentField="country"
-          />
-          <SplineSeries
-            name="Oil"
-            valueField="oil"
-            argumentField="country"
-          />
-          <SplineSeries
-            name="Natural gas"
-            valueField="gas"
-            argumentField="country"
-          />
-          <SplineSeries
-            name="Coal"
-            valueField="coal"
-            argumentField="country"
-          />
-          <SplineSeries
-            name="Nuclear"
-            valueField="nuclear"
-            argumentField="country"
-          />
-          <Legend
-            position="bottom"
-            rootComponent={LegendRoot}
-            itemComponent={LegendItem}
-            labelComponent={this.legendLabel}
-          />
-
-          <EventTracker />
-          <HoverState hover={hover} onHoverChange={this.changeHover} />
-        </Chart>
-      </Paper>
-    );
-  }
-}
+        <EventTracker />
+        <HoverState hover={hover} onHoverChange={changeHover} />
+      </Chart>
+    </Paper>
+  );
+};
